e030f34eb140b0ca99d187f4d50fc3b607048839
kady39598-bit <kady39598@gmail.com>
2026-05-02 18:10:56 -0300

    Create astra_database.gd

---
+++ scripts/astra_database.gd
@@ -0,0 +1,9 @@
+extends Node
+
+var especies = [
+	{"nome":"Scorpadeus","tipo":"terra"},
+	{"nome":"Lupyx","tipo":"terra"},
+	{"nome":"Leonyx","tipo":"terra"},
+	{"nome":"Dracoryx","tipo":"voador"},
+	{"nome":"Astryx","tipo":"cosmico"}
+]
