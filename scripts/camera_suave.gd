82b363bb7d23df0883ca6fb0a461e2d69906965f
kady39598-bit <kady39598@gmail.com>
2026-05-05 20:39:58 -0300

    Create camera_suave.gd

---
+++ scripts/camera_suave.gd
@@ -0,0 +1,7 @@
+extends Camera2D
+
+func _process(_delta):
+    # Se a câmera for filha do Player, ela já segue sozinha.
+    # Este script serve para você ajustar o zoom ou limites depois.
+    pass
+
